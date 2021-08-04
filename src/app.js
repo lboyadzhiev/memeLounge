22c6d1190e02eecbb5cd36b758c8ec3ec5b1484f
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-08-04 20:43:07 +0300

    api module

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js';
+
+import { homePage } from './views/home.js';
+
+const main = document.querySelector('main');
+setUserNav();
+
+page('/', decorateContext, homePage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
+
+    next();
+}
+
+function setUserNav() {
+    const token = sessionStorage.getItem('authToken');
+
+    if (token != null) {
+        document.querySelector('.user').style.display = '';
+        document.querySelector('.guest').style.display = 'none';
+    } else {
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = '';
+    }
+}
